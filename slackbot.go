aee4004c1721ffd09cfc1843bdf39f4f1749558b
Regis Perez <regisbatistaperez@gmail.com>
2018-09-26 11:18:50 -0300

    Slackbot Refatorado

--- slackbot.go
+++ slackbot.go
@@ -16,118 +16,67 @@ func slackbot() {
 	port := map[bool]string{true: os.Getenv("PORT"), false: "8080"}[os.Getenv("PORT") != ""]
 	go http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	api := slack.New("xoxb-379678856742-414621599025-kscIpN2XZgX6vRNwlkQHT1jC")
-	ontem, hoje, impedimento := "", "", ""
+	perguntas := []string{"O que você fez ontem?", "O que você fará hoje?", "Existe algum impedimento na sua tarefa?"}
+	respostas := make([]string, len(perguntas))
+	canal := "infinitydaily"
+	acao := " postou no Infinity Daily Standup"
+	mensagemFinal := "Bom trabalho!"
+	mensagemCancelamento := "Report Cancelado"
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-			if len(ev.User) == 0 {
-				continue
-			}
 			if ev.Text == "cancel" || ev.Text == "Cancel" {
-				user, err := api.GetUserInfo(ev.User)
-				_, _, channelID, err := api.OpenIMChannel(user.ID)
-				if err != nil {
-					fmt.Printf("[WARN]  could not grab user information: %s", ev.User)
-				}
-				api.PostMessage(channelID, "Report cancelado", slack.PostMessageParameters{})
+				user, _ := api.GetUserInfo(ev.User)
+				_, _, channelID, _ := api.OpenIMChannel(user.ID)
+				api.PostMessage(channelID, mensagemCancelamento, slack.PostMessageParameters{})
+				slackbot()
 			}
 			if ev.Text == "report" || ev.Text == "Report" {
-				user, err := api.GetUserInfo(ev.User)
-				_, _, channelID, err := api.OpenIMChannel(user.ID)
-				if err != nil {
-					fmt.Printf("[WARN]  could not grab user information: %s", ev.User)
-				}
-				api.PostMessage(channelID, "O que você fez ontem?", slack.PostMessageParameters{})
-				for msg := range rtm.IncomingEvents {
-					switch ev := msg.Data.(type) {
-					case *slack.MessageEvent:
-						if ev.Text == "cancel" || ev.Text == "Cancel" {
-							user, err := api.GetUserInfo(ev.User)
-							_, _, channelID, err := api.OpenIMChannel(user.ID)
-							if err != nil {
-								fmt.Printf("[WARN]  could not grab user information: %s", ev.User)
+				for i := 0; i < len(perguntas); i++ {
+					respondeu := false
+					user, _ := api.GetUserInfo(ev.User)
+					_, _, channelID, _ := api.OpenIMChannel(user.ID)
+					api.PostMessage(channelID, perguntas[i], slack.PostMessageParameters{})
+					for msg := range rtm.IncomingEvents {
+						switch ev := msg.Data.(type) {
+						case *slack.MessageEvent:
+							if ev.Text == "cancel" || ev.Text == "Cancel" {
+								user, _ := api.GetUserInfo(ev.User)
+								_, _, channelID, _ := api.OpenIMChannel(user.ID)
+								api.PostMessage(channelID, mensagemCancelamento, slack.PostMessageParameters{})
+								slackbot()
 							}
-							api.PostMessage(channelID, "Report cancelado", slack.PostMessageParameters{})
-							slackbot()
-						}
-						if ev.Text != "O que você fez ontem?" && ev.Text != "cancel" {
-							ontem = ev.Text
-						}
-						if len(ontem) > 0 {
-							api.PostMessage(channelID, "O que você fará hoje?", slack.PostMessageParameters{})
-							for msg := range rtm.IncomingEvents {
-								switch ev := msg.Data.(type) {
-								case *slack.MessageEvent:
-									if ev.Text == "cancel" || ev.Text == "Cancel" {
-										user, err := api.GetUserInfo(ev.User)
-										_, _, channelID, err := api.OpenIMChannel(user.ID)
-										if err != nil {
-											fmt.Printf("[WARN]  could not grab user information: %s", ev.User)
-										}
-										api.PostMessage(channelID, "Report cancelado", slack.PostMessageParameters{})
-										slackbot()
-									}
-									if ev.Text != "O que você fará hoje?" && ev.Text != "cancel" {
-										hoje = ev.Text
-									}
-									if len(hoje) > 0 {
-										api.PostMessage(channelID, "Existe algum impedimento na sua tarefa?", slack.PostMessageParameters{})
-										for msg := range rtm.IncomingEvents {
-											switch ev := msg.Data.(type) {
-											case *slack.MessageEvent:
-												if ev.Text == "cancel" || ev.Text == "Cancel" {
-													user, err := api.GetUserInfo(ev.User)
-													_, _, channelID, err := api.OpenIMChannel(user.ID)
-													if err != nil {
-														fmt.Printf("[WARN]  could not grab user information: %s", ev.User)
-													}
-													api.PostMessage(channelID, "Report cancelado", slack.PostMessageParameters{})
-													slackbot()
-												}
-												if ev.Text != "Existe algum impedimento na sua tarefa?" && ev.Text != "cancel" {
-													impedimento = ev.Text
-													if len(impedimento) > 0 {
-														api.PostMessage(channelID, "Bom trabalho!", slack.PostMessageParameters{})
-														params := slack.PostMessageParameters{}
-														attachment := slack.Attachment{
-
-															Fields: []slack.AttachmentField{
-																slack.AttachmentField{
-																	Title: "O que você fez ontem?",
-																	Value: ontem,
-																},
-																slack.AttachmentField{
-																	Title: "O que você fará hoje?",
-																	Value: hoje,
-																},
-																slack.AttachmentField{
-																	Title: "Existe algum impedimento na sua tarefa?",
-																	Value: impedimento,
-																},
-															},
-														}
-														params.Attachments = []slack.Attachment{attachment}
-														params.AsUser = false
-														params.IconURL = user.Profile.Image48
-														params.Username = user.RealName
-														api.PostMessage("infinitydaily", user.RealName+" postou no Infinity Daily Standup", params)
-														slackbot()
-													}
-												}
-											}
-										}
-									}
-								}
+							if ev.Text != perguntas[i] {
+								respostas[i] = ev.Text
+								respondeu = true
 							}
+						}
+						if respondeu == true {
+							break
 						}
 					}
 				}
+				if respostas[len(respostas)-1] != "" {
+					user, _ := api.GetUserInfo(ev.User)
+					_, _, channelID, _ := api.OpenIMChannel(user.ID)
+					api.PostMessage(channelID, mensagemFinal, slack.PostMessageParameters{})
+					params := slack.PostMessageParameters{}
+					fields := make([]slack.AttachmentField, len(perguntas))
+					for i := 0; i < len(perguntas); i++ {
+						fields[i].Title = perguntas[i]
+						fields[i].Value = respostas[i]
+					}
+					attachment := slack.Attachment{Fields: fields}
+					params.Attachments = []slack.Attachment{attachment}
+					params.AsUser = false
+					params.IconURL = user.Profile.Image48
+					params.Username = user.RealName
+					api.PostMessage(canal, user.RealName+acao, params)
+					slackbot()
+				}
 			}
-
-		case *slack.RTMError:
-			fmt.Printf("[ERROR] %s\n", ev.Error())
 		}
 	}
 }
